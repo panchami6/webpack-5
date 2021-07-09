f15c56e00206816be8e4caeacda744f3813c470c
panchami6 <62180655+panchami6@users.noreply.github.com>
2021-07-09 23:20:41 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,42 +1,27 @@
 import Logo from "./logo.svg";
 
-const root = document.getElementById("root");
-const name = "Tanay";
-let route;
+let root;
 
 function init() {
-	root.innerHTML = `
-        <img src="${Logo}" alt="build icon" />
-        <h1> vanillaJS app Builder </h1>
-        <button id="about"> About </button>
-        <button id="programs"> Programs </button> <!-- homework -->
+    root = document.getElementById("root");
+    root.innerHTML = `
+        <img src="${Logo}" alt="builder icon" />
+        <h1>learning web tooling today</h1>
+        <button id= "about">About</button>
         <div id="route"></div>
 `;
-
-	route = document.querySelector("#route");
-
-	document.querySelector("#about").addEventListener("click", showAboutPage);
 }
 
-function showLoader(root) {
-	const loader = document.createElement("div");
-	loader.innerText = "loading..";
-	root.appendChild(loader);
-	return loader;
+init()
+
+const button = document.querySelector("#about")
+button.addEventListener("click", loadAbout)
+
+function loadAbout() {
+    import("./App").then(({ default: App}) => {
+        const about = App();
+
+        document.querySelector("#route").innerHTML = about;
+    } )
 }
 
-function hideLoader(root, loader) {
-	root.removeChild(loader);
-}
-
-function showAboutPage(event) {
-	const loader = showLoader(root);
-
-    // THIS IS THE MAIN CODE FOR LAZY LOADING
-	import(/* webpackPrefetch: true */ "./App").then(({ default: App }) => {
-		hideLoader(root, loader);
-		route.innerHTML = App({ name });
-	});
-}
-
-init();
--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,50 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpackConfig = {
 	entry: path.resolve(__dirname, "src", "index.js"),
 
-	output: {
-		filename: "[name].[contenthash].bundle.js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true
-	},
 	devtool: "inline-source-map",
+
 	devServer: {
 		contentBase: "./dist"
 	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"]
-					}
-				}
-			},
-			{
+
+    output: {
+        filename: "[name].[contenthash].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
 				test: /\.css$/i,
 				use: ["style-loader", "css-loader"]
-			},
-			{
+			
+            },
+            {
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: "asset"
-			}
-		]
-	},
+            }
+        ]
+    },
 
-	plugins: [
+    plugins: [
 		new HtmlWebpackPlugin({
 			title: "vanillaJS app",
 			template: path.resolve(__dirname, "src", "index.html")
 		})
 	],
 
-	optimization: {
+    optimization: {
 		splitChunks: {
 			cacheGroups: {
 				node_vendors: {
@@ -59,4 +63,7 @@ const webpackConfig = {
 	mode: "production"
 };
 
+
+
 module.exports = webpackConfig;
+
